31dd9c2a5cc927bb29f13ed8f568cc00b69efe27
kaxla <kaylas.email.address@gmail.com>
2014-01-21 20:23:02 -0800

    write psuedocode for extensible version

---
+++ extensible_fizbuzz.rb
@@ -0,0 +1,15 @@
+class FizzBuzz
+  def initialize()
+
+end
+
+
+given a range of numbers and the numbers you want to divide by/what their word will be
+
+take in a max number
+
+take in a list of divisors
+
+iterate through each value up to max number checking for divisibility by divisors
+
+
--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,14 @@
 def simple_fizz_buzz(number)
-number = Integer(gets.chomp)
-    if number % 3 == 0 && number % 5 == 0
-      puts "fizzbuzz"
-    elsif number % 3 == 0
-      puts "fizz"
-    elsif number % 5 == 0
-      puts "buzz"
-    else
-      puts number
-    end
+  if number % 3 == 0 && number % 5 == 0
+    puts "fizzbuzz"
+  elsif number % 3 == 0
+    puts "fizz"
+  elsif number % 5 == 0
+    puts "buzz"
+  else
+    puts number
   end
+end
 
 
 
